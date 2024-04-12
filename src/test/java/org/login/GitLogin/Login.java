0fc2523cf3f8237112aca8d18ff82caaeeb93c41
keerthu1808 <keerthu180895@gmail.com>
2024-04-12 11:38:16 +0530

    Login

---
+++ src/test/java/org/login/GitLogin/Login.java
@@ -0,0 +1,15 @@
+package org.login.GitLogin;
+
+public class Login {
+
+	private void loginMeth() {
+		
+		System.out.println("UserName");
+		System.out.println("Password");
+	}
+	
+	private void click() {
+		
+		System.out.println("Login");
+	}
+}
